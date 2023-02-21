ff3a607add80a063c4117ce728b7527954fd7c37
Do Tuan Dat <dathymax@gmail.com>
2023-02-22 00:09:58 +0700

    remove .vscode

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import HomePage from "@/components/home";
-
-export default function Home() {
-    return (
-        <main className="md:container m-auto">
-            <HomePage />
-        </main>
-    )
-}

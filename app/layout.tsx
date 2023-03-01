203782eda9aacc0a54290bb57f066f01df897aa5
Do Tuan Dat <dathymax@gmail.com>
2023-03-01 20:59:41 +0700

    remove head.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import Navigation from '@/components/navigation/Navigation';
 import './globals.css'
 
+export const metadata = {
+    title: 'Travel Social Network',
+    description: 'Travel Social Network',
+}
+
 export default function RootLayout({
     children,
 }: {
@@ -12,7 +17,9 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-            <head />
+            <head>
+                <meta content="width=device-width, initial-scale=1" name="viewport" />
+            </head>
             <body className="md:container m-auto">
                 <Navigation />
                 <div className="pt-20">

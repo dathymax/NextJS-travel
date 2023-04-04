c3c3d3b6745081ee6e915b4dbf19d957bd0da8c6
Đỗ Tuấn Đạt <datdt@dogoo.com.vn>
2023-04-04 15:41:28 +0700

    update layout payment

---
+++ components/features/product/pay/heading/index.tsx
@@ -0,0 +1,37 @@
+import Rating from '@/components/common/rating';
+import React from 'react';
+
+function ProductPayHeading() {
+    return (
+        <div className='flex items-center gap-6'>
+            <img src="/assets/images/nb-1.jpg" alt="mock" className='w-[170px] h-[170px] rounded-xl' />
+
+            <div>
+                <p className='text-black font-bold'>
+                    Spectacular views of Queenstown
+                </p>
+
+                <div className="flex items-center text-[12px]">
+                    <span className='text-gray-500'>
+                        Hosted by
+                    </span>
+                    <img src="/assets/mock/avatar.jpg" alt="avatar" className='w-[20px] h-[20px] rounded-full mx-1' />
+                    <span className="text-black font-bold">
+                        Zoe Towne
+                    </span>
+                </div>
+
+                <div className="flex items-center gap-3 my-3 text-[12px]">
+                    <span className='text-gray-500'>1 private bath</span>
+                    <span className='text-gray-500'>1 bedroom</span>
+                </div>
+
+                <hr className='my-3' />
+
+                <Rating />
+            </div>
+        </div>
+    );
+}
+
+export default ProductPayHeading;

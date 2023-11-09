17113555bc3161bf1e7283aebff5820afde48248
Do Tuan Dat <dathymax@gmail.com>
2023-11-09 22:51:11 +0700

    update

--- components/layouts/theme/index.tsx
+++ components/layouts/theme/index.tsx
@@ -20,7 +20,7 @@ const Theme: React.FC = () => {
             inputColor.type = "color";
             inputColor.click();
             inputColor.addEventListener("change", () => {
-                setRgb(hexToRgb(inputColor.value).toString())
+                // setRgb(hexToRgb(inputColor.value).toString())
                 setName(color.name)
             })
         } else {
@@ -29,9 +29,9 @@ const Theme: React.FC = () => {
         }
     }
 
-    function hexToRgb(hex: string) {
-        return ["0x" + hex[1] + hex[2] | 0, "0x" + hex[3] + hex[4] | 0, "0x" + hex[5] + hex[6] | 0]
-    }
+    // function hexToRgb(hex: string) {
+    //     return ["0x" + hex[1] + hex[2] | 0, "0x" + hex[3] + hex[4] | 0, "0x" + hex[5] + hex[6] | 0]
+    // }
 
     useEffect(() => {
         if (!rgb || !name) return;

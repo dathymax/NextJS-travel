0cb7115328cbd116e3b2a5dc623358d2e8844faa
Do Tuan Dat <dathymax@gmail.com>
2023-11-09 21:44:08 +0700

    update

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -3,14 +3,8 @@ import Trending from '@/components/sections/trending';
 import React from 'react'
 import tour from "@/mock/data/tour.json"
 
-export async function getTours() {
-    const tourList = tour;
-
-    return tourList;
-}
-
 const ToursPage = async () => {
-    const tourList = await getTours();
+    const tourList = tour;
 
     return (
         <div>

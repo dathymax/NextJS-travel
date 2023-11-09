619c3c5b430fdd33cb0568df413c4566523d83af
Do Tuan Dat <dathymax@gmail.com>
2023-11-09 20:54:27 +0700

    update

--- app/account-setting/page.tsx
+++ app/account-setting/page.tsx
@@ -1,20 +1,14 @@
 import AccountSetting from '@/components/features/account-setting'
 import React from 'react'
 
-export async function getUserInfo() {
-    const data = {
+const AccountSettingPage = async () => {
+    const userInfo = {
         displayName: "Jtadd",
         realName: "Do Tuan Dat",
         phone: "0984193954",
         email: "dathymax@gmail.com",
         bio: "Happiness looks greater on you <3"
-    }
-
-    return data
-}
-
-const AccountSettingPage = async () => {
-    const userInfo = await getUserInfo();
+    };
 
     return (
         <AccountSetting userInfo={userInfo} />
